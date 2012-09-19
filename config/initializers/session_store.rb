8129c8c46fcfa2cfeca1edada01a0befb6fddd3d
Kelly Becker <kellylsbkr@gmail.com>
2012-09-19 15:17:32 -0700

    mysql sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Preview::Application.config.session_store :cookie_store, key: '_preview_session'
+Preview::Application.config.session_store :active_record_store
+#Preview::Application.config.session_store :cookie_store, key: '_preview_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
