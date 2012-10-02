4cbaca684e51b63b61e481c1f0789838d2a58e2b
Kelly Becker <kellylsbkr@gmail.com>
2012-10-02 12:33:20 -0700

    a fix to the profile page involving style when logged out

--- db/schema.rb
+++ db/schema.rb
@@ -224,6 +224,10 @@ ActiveRecord::Schema.define(:version => 20121002003523) do
     t.integer  "teacher_id"
   end
 
+  create_table "helpful_queries", :force => true do |t|
+    t.string "query"
+  end
+
   create_table "interviews", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -519,7 +523,7 @@ ActiveRecord::Schema.define(:version => 20121002003523) do
     t.boolean  "currently_seeking",                       :default => true
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "url"
+    t.string   "url",                                     :default => ""
     t.string   "resume_file_name"
     t.string   "resume_content_type"
     t.integer  "resume_file_size"
@@ -567,9 +571,9 @@ ActiveRecord::Schema.define(:version => 20121002003523) do
     t.boolean  "is_shared",           :default => false, :null => false
     t.boolean  "is_limited",          :default => false, :null => false
     t.boolean  "emailsubscription",   :default => true
+    t.string   "time_zone",           :default => "UTC"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "time_zone",           :default => "UTC"
     t.boolean  "emaileventreminder"
     t.boolean  "emaileventapproved"
     t.string   "original_name"
@@ -613,6 +617,7 @@ ActiveRecord::Schema.define(:version => 20121002003523) do
     t.integer  "duration_in_ms"
     t.string   "aspect_ratio"
     t.boolean  "is_snippet",             :default => false,       :null => false
+    t.string   "video"
   end
 
   add_index "videos", ["teacher_id"], :name => "index_videos_on_teacher_id"
