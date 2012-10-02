0206260dba5b96743e42fa8192273b0ea386e834
Kelly Becker <kellylsbkr@gmail.com>
2012-10-01 17:51:51 -0700

    jcrop chmod

--- config/rubber/deploy-util.rb
+++ config/rubber/deploy-util.rb
@@ -94,4 +94,11 @@ namespace :rubber do
   task :rake do
     rsudo "cd #{current_release} && RAILS_ENV=#{Rubber.env} #{fetch(:rake, 'rake')} " + ENV['COMMAND']
   end
+
+  desc <<-DESC
+    Chmod a directory in the rails root
+  DESC
+  task :chmod do
+    rsudo "cd #{current_release} && RAILS_ENV=#{Rubber.env} chmod -Rf ./" + ENV['CHMOD']
+  end
 end
