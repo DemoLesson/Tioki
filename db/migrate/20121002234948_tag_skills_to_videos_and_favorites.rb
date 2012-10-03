866f5517246d81a74d2aa7b33e0ddcce587c6646
Kelly Becker <kellylsbkr@gmail.com>
2012-10-02 17:30:32 -0700

    save

--- db/migrate/20121002234948_tag_skills_to_videos_and_favorites.rb
+++ db/migrate/20121002234948_tag_skills_to_videos_and_favorites.rb
@@ -1,17 +1,17 @@
 class TagSkillsToVideosAndFavorites < ActiveRecord::Migration
 	def up
-    	create_table :video_favorites, :id => false do |t|
+    	create_table :videos_favorites, :id => false do |t|
       		t.column :video_id, :integer
       		t.column :teacher_id, :integer
     	end
-    	create_table :video_skills, :id => false do |t|
+    	create_table :videos_skills, :id => false do |t|
       		t.column :video_id, :integer
       		t.column :skill_id, :integer
     	end
   	end
 
   	def down
-  		drop_table :video_favorites
-  		drop_table :video_skills
+  		drop_table :videos_favorites
+  		drop_table :videos_skills
   	end
 end
