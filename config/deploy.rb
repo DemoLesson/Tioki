7cc54cfe4c6a6f7c05b59f206c57f375b5e30fe1
Kelly Becker <kellylsbkr@gmail.com>
2012-10-10 14:41:47 -0700

    deploy script updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,6 +80,7 @@ end
 # capistrano's deploy:cleanup doesn't play well with FILTER
 after "deploy", "cleanup"
 after "deploy:migrations", "cleanup"
+
 task :cleanup, :except => { :no_release => true } do
   count = fetch(:keep_releases, 5).to_i
   
@@ -91,12 +92,30 @@ task :cleanup, :except => { :no_release => true } do
   CMD
 end
 
-#if Rubber::Util.has_asset_pipeline?
-#  # load asset pipeline tasks, and reorder them to run after
-#  # rubber:config so that database.yml/etc has been generated
-#  load 'deploy/assets'
-#  callbacks[:after].delete_if {|c| c.source == "deploy:assets:precompile"}
-#  callbacks[:before].delete_if {|c| c.source == "deploy:assets:symlink"}
-#  before "deploy:assets:precompile", "deploy:assets:symlink"
-#  after "rubber:config", "deploy:assets:precompile"
-#end
+if Rubber::Util.has_asset_pipeline?
+  # load asset pipeline tasks, and reorder them to run after
+  # rubber:config so that database.yml/etc has been generated
+  load 'deploy/assets'
+
+  # Delete preloaded callbacks
+  after = ["deploy:assets:precompile"]
+  before = ["deploy:assets:symlink", "rubber:config"]
+  after = callbacks[:after].delete_if {|c| after.include? c.source}
+  before = callbacks[:before].delete_if {|c| before.include? c.source}
+
+  # Debugging code
+  #after.each{ |x|
+  #  next if x.class.name == 'Capistrano::ProcCallback'
+  #  puts x.only.first + " --> " + x.source
+  #}
+  #exit
+
+  # Before we precompile link assets to shared
+  before "deploy:assets:precompile", "deploy:assets:symlink"
+
+  # Dont precompile until rubber config has been run
+  after "rubber:config", "deploy:assets:precompile"
+
+  # After precompile migrate the database
+  after "deploy:assets:precompile", "deploy:db:migrate"
+end
