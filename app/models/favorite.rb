46b047ece28c2fccd27ad03c4c04591db90c6439
Kelly Becker <kellylsbkr@gmail.com>
2012-10-20 12:37:09 -0700

    favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,31 @@
 class Favorite < ActiveRecord::Base
 	belongs_to :user
 
-	def model
-		_map!(super)
+	def model(sup = false)
+		return super() if sup
+		return _map!(super()) unless sup
+	end
+
+	def model?(*classes)
+
+		# Get model
+		comp = model(true)
+
+		# IDK why i have to use this
+		comp = comp.class.name unless comp.is_a?(String)
+
+		# Get model class name
+		comp = comp.split(':').first
+
+		# Is the model any of the following
+		classes.each do |_class|
+			_class = _class.to_s if _class.is_a?(Symbol)
+
+			# If the comparision is true
+			return true if comp.downcase == _class.downcase
+		end
+
+		# Return false
+		return false
 	end
 end
