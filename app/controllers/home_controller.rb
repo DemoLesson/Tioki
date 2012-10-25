c21a4779d15a25c15abcaf9e344c99c360209c91
Aleks Stanisic <astan@demolesson.com>
2012-10-24 19:45:27 -0700

    changing suggested connections number to 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -48,7 +48,7 @@ class HomeController < ApplicationController
         @suggested_connections = User.joins(joins.join(" ")).where("`users`.`avatar_file_size` IS NOT NULL")
         @suggested_connections = @suggested_connections.where("`connections`.`owned_by` NOT IN (?)", my_connections)
         @suggested_connections = @suggested_connections.where("`connections`.`user_id` NOT IN (?)", my_connections)
-        @suggested_connections = @suggested_connections.select('`users`.*').group('`users`.`id`').limit(5).order('(RAND() / COUNT(*) * 2)')
+        @suggested_connections = @suggested_connections.select('`users`.*').group('`users`.`id`').limit(3).order('(RAND() / COUNT(*) * 2)')
 
         @latest_dl = Whiteboard.where("`slug` = ?", 'video_upload').order('`created_at`').limit(3)
 
