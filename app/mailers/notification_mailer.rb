0c7013b87fba636ccc8eddd1fdf87a8e06d0608c
Elijah Green <elijahgreen@gmail.com>
2012-10-30 11:16:52 -0700

    Add functions for notification mailers

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,59 @@
+class NotificationMailer < ActionMailer::Base
+	default :from => "Tioki <tioki@tioki.com>"
+	def comments(user, commentable)
+		@user= user
+
+		#Was this created, participanted in, or followed in that order
+		@type = commentable.class.name
+		if @type == "Discussion"
+		end
+
+		mail = mail(:to => @user.email, :subject => '#{} and #{} other commented on your #{@type} Post')
+
+		if mail.delivery_method.respond_to?('tag')
+			mail.delivery_method.tag("multiple_comments_#{@type.downcase}")
+		end
+
+		return mail
+	end
+
+	def comment(user, commentable)
+		@user= user
+
+		@type = commentable.class.name
+
+		#Was this created, participanted in, or followed in that order
+		if @type == "Discussion"
+		end
+
+		mail = mail(:to => @user.email, :subject => "#{} commented on our #{@type} Post")
+
+		if mail.delivery_method.respond_to?('tag')
+			mail.delivery_method.tag("one_comment_#{@type.downcase}")
+		end
+
+		return mail
+	end
+
+	def likes
+		mail = mail(:to => @user.email, :subject => "#{} and #{} others liked your post")
+
+		if mail.delivery_method.respond_to?('tag')
+			mail.delivery_method.tag('multiple_likes')
+		end
+
+		return mail
+	end
+
+	def like(liker, likee)
+		@liker = liker
+
+		mail = mail(:to => @user.email, :subject => '#{} liked your post.')
+
+		if mail.delivery_method.respond_to?('tag')
+			mail.delivery_method.tag('one_like')
+		end
+
+		return mail
+	end
+end
