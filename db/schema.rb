4ee4d73e66cdafd9a16fed8911c980e7d44d32ec
Kelly Becker <kellylsbkr@gmail.com>
2012-11-09 12:36:53 -0800

    comments redesign

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121107031345) do
+ActiveRecord::Schema.define(:version => 20121108001531) do
 
   create_table "abtests", :force => true do |t|
     t.string  "slug"
