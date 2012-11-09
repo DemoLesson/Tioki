385422894ffaaec4463489e74e8af6493e3c2e77
Elijah Green <elijahgreen@gmail.com>
2012-11-09 14:47:45 -0800

    Add get started link to the profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -590,6 +590,7 @@ class UsersController < ApplicationController
 	def tioki_coins_list
 		#get users tioki dollars except from those that received
 		#them solely from getting started which depends on 6 different tables
+		#TODO set time for tioki_bucks start
 		connection_invites = ConnectionInvite.find(:all, :conditions => ["connection_invites.created_at > ?", 1.hour.ago]).collect(&:user_id)
 		@teachers = Teacher.find(:all, :conditions => ["teachers.facebook_connect = true || teachers.twitter_connect = true || teachers.tweet_about = true || teachers.user_id IN (?)", connection_invites])
 	end
