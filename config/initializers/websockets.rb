427f0bf99ad36a2ec7df3fbd9a16534df31bbf83
Kelly Becker <kellylsbkr@gmail.com>
2012-11-10 16:42:48 -0800

    I think i got websockets working!

--- config/initializers/websockets.rb
+++ config/initializers/websockets.rb
@@ -1,7 +1,10 @@
 require 'em-websocket'
 
-EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 7777) do |ws|
-  ws.onopen    { ws.send "Hello Client!"}
-  ws.onmessage { |msg| ws.send "Pong: #{msg}" }
-  ws.onclose   { puts "WebSocket closed" }
+# Set up a websocket server in a thread
+WS = Thread.new() do
+	EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 7777) do |ws|
+	  ws.onopen    { ws.send "Hello Client!"}
+	  ws.onmessage { |msg| ws.send "Pong: #{msg}" }
+	  ws.onclose   { puts "WebSocket closed" }
+	end
 end
