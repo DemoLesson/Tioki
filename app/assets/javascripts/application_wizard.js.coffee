b479ee6ab458156bf0fa1cc724b20406ad19fa7b
Kelly Becker <kellylsbkr@gmail.com>
2012-11-19 10:51:10 -0800

    progress;

---
+++ app/assets/javascripts/application_wizard.js.coffee
@@ -0,0 +1,5 @@
+$ ->
+	# Step 1
+
+	# Step 2
+	do $('input.datepicker').datepicker
--- app/controllers/application_wizard_controller.rb
+++ app/controllers/application_wizard_controller.rb
@@ -6,12 +6,31 @@ class ApplicationWizardController < ApplicationController
 
 	def step1
 		if request.post?
-			dump params
-
 			user = User.create(params[:user])
 			session[:user] = User.authenticate(user.email, user.password)
 
 			return redirect_to :step2
+		end
+	end
+
+	def step2
+		if request.post?
+			user = User.current
+
+			if params[:experience][:current]
+				params[:experience][:current] = true
+			else
+				params[:experience][:current] = false
+			end
+
+			end_date = Time.strptime(params[:date][:end_date], "%m/%d/%Y")
+			start_date = Time.strptime(params[:date][:start_date], "%m/%d/%Y")
+			params[:experience][:startMonth] = start_date.strftime("%m")
+			params[:experience][:startYear] = start_date.strftime("%Y")
+			params[:experience][:endMonth] = end_date.strftime("%m")
+			params[:experience][:endYear] = end_date.strftime("%Y")
+			exp = Experience.create(params[:experience])
+			user.teacher.experiences << exp
 		end
 	end
 
