a407ca0d6289caedd6c53390598f53cc2dfbea55
Kelly Becker <kellylsbkr@gmail.com>
2012-11-20 10:22:15 -0800

    destory video exceptions

--- app/models/video.rb
+++ app/models/video.rb
@@ -217,10 +217,14 @@ class Video < ActiveRecord::Base
 
       # Get the API Response
       response = details(width, height, output_url)
-      self.destroy if response["error"] == "Not Found"
       
-      # Return HTML Embed Code
-      return response["html"].html_safe
+      begin
+        # Return HTML Embed Code
+        return response["html"].html_safe
+      rescue
+        self.destroy
+        return nil
+      end
     end
 
     if job_status == 'finished'
