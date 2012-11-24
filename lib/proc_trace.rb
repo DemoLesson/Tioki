d9e47ca0aebfed4a525722692f78177036b2a3b6
Kelly Becker <kellylsbkr@gmail.com>
2012-11-24 11:29:02 -0800

    ruby active trace

--- lib/proc_trace.rb
+++ lib/proc_trace.rb
@@ -1,8 +1,10 @@
-set_trace_func proc { |event, file, line, id, binding, classname|
-  # only interested in events of type 'call' (Ruby method calls)
-  # see the docs for set_trace_func for other supported event types
+if ENV['RUBY_DEBUG'] == 'true'
+	set_trace_func proc { |event, file, line, id, binding, classname|
+	  # only interested in events of type 'call' (Ruby method calls)
+	  # see the docs for set_trace_func for other supported event types
 
-  #printf "%8s %s:%-2d %10s %8s\n", event, file, line, id, classname
-  #    call prog.rb:2        test     Test
-  puts "\e[31mMethod Call: `#{classname}.#{id}` called\n\tin #{file}:#{line}.\n\e[0m" if event == 'call'
-}
+	  #printf "%8s %s:%-2d %10s %8s\n", event, file, line, id, classname
+	  #    call prog.rb:2        test     Test
+	  puts "\e[31mMethod Call: `#{classname}.#{id}` called\n\tin #{file}:#{line}.\n\e[0m" if event == 'call'
+	}
+end
