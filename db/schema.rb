ee72216708383a8f7c8d2a6e87f044f6f78e8802
Kelly Becker <kellylsbkr@gmail.com>
2012-11-28 13:00:06 -0800

    commented out connection degree

--- db/schema.rb
+++ db/schema.rb
@@ -355,6 +355,10 @@ ActiveRecord::Schema.define(:version => 20121128000713) do
     t.string   "facebook"
   end
 
+  create_table "helpful_queries", :force => true do |t|
+    t.string "query"
+  end
+
   create_table "interviews", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -663,7 +667,7 @@ ActiveRecord::Schema.define(:version => 20121128000713) do
     t.boolean  "currently_seeking",                       :default => true
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "url"
+    t.string   "url",                                     :default => ""
     t.string   "resume_file_name"
     t.string   "resume_content_type"
     t.integer  "resume_file_size"
