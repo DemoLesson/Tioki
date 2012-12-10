a0777799acb23f5e93a28c377b81e239c97861fd
Elijah Green <elijahgreen@gmail.com>
2012-12-10 10:23:50 -0800

    Fix a broken migration

---
+++ app/models/teacher.rb
@@ -0,0 +1,8 @@
+class Teacher < ActiveRecord::Base
+	#Without this file the merge_teachers.rake file is broken
+	#which is called by the merge_teacher_users migration
+	#DON'T DELETE this file until that file is changed to
+	#use only sql
+	
+	belongs_to :user
+end
