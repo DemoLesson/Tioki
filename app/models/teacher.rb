f79e15112acd561a85cb083f2eeb7a895744b6dd
Elijah Green <elijahgreen@gmail.com>
2012-12-11 11:21:30 -0800

    Clean up a fix to migration
    Also commenting  out the rake task in a migration so they it can
    actaully function.

--- app/models/teacher.rb
+++
@@ -1,9 +0,0 @@
-class Teacher < ActiveRecord::Base
-	#Without this file the merge_teachers.rake file is broken
-	#which is called by the merge_teacher_users migration
-	#DON'T DELETE this file until that file is changed to
-	#use only sql or the rake task is removed fromt the
-	#migration
-	
-	belongs_to :user
-end
