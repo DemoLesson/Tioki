0119577e264510dc920130edb7982a446d83080f
Elijah Green <elijahgreen@gmail.com>
2012-12-11 16:46:35 -0800

    Don't allow urls to be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,6 +57,7 @@ class User < ActiveRecord::Base
 	has_and_belongs_to_many :grades
 	validates_associated :assets
 	validates_uniqueness_of :slug, :message => "That profile url is not available"
+	validates_presence_of :slug, :on => :update
 
 	# Has Many Connections
 	has_many :activities, :order => 'created_at DESC'
