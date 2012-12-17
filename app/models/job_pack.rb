7bfdec86641327df9a58dfc886879c8a76b8d651
Kelly Becker <kellylsbkr@gmail.com>
2012-12-17 12:04:17 -0800

    added group_id to accessible

--- app/models/job_pack.rb
+++ app/models/job_pack.rb
@@ -1,5 +1,5 @@
 class JobPack < ActiveRecord::Base
-	attr_accessible :group, :jobs, :expiration, :inception, :charge_token, :card_token, :refunded, :amount, :additiona_data
+	attr_accessible :group, :group_id, :jobs, :expiration, :inception, :charge_token, :card_token, :refunded, :amount, :additiona_data
 
 	belongs_to :group
 
