d9a15dc3814c14b9640effab3adb9839b07815d4
Kelly Becker <kellylsbkr@gmail.com>
2013-01-04 10:45:18 -0800

    application flow additions

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,56 +1,56 @@
 class Application < ActiveRecord::Base
-  has_many :assets, :dependent => :destroy
-  belongs_to :job
+	has_many :assets, :dependent => :destroy
+	belongs_to :job
 	belongs_to :user
 
-  has_one :video
-  has_one :interview, :dependent => :destroy
+	belongs_to :video
+	has_one :interview, :dependent => :destroy
 
-  scope :is_active, where(:status => 1)
+	scope :is_active, where(:status => 1)
 	scope :is_submitted, where(:submitted => 1)
 	scope :not_rejected, where("status is null || status != 'Deny Application'")
 
-  def self.mine(args = {})
+	def self.mine(args = {})
 
-    # Set the user to lookup
-    a = User.current.id if args[:user].nil?
-    a = args[:user] unless args[:user].nil?
+		# Set the user to lookup
+		a = User.current.id if args[:user].nil?
+		a = args[:user] unless args[:user].nil?
 
-    # Get all my applications
-    tmp = self.where('`user_id` = ?', a)
+		# Get all my applications
+		tmp = self.where('`user_id` = ?', a)
 
-    # Filter down
-    tmp = tmp.where('`viewed` = ?', args[:viewed]) unless args[:viewed].nil?
-    tmp = tmp.where('`status` = ?', args[:status]) unless args[:status].nil?
-    tmp = tmp.where('`status` != ?', args[:status!]) unless args[:status!].nil?
+		# Filter down
+		tmp = tmp.where('`viewed` = ?', args[:viewed]) unless args[:viewed].nil?
+		tmp = tmp.where('`status` = ?', args[:status]) unless args[:status].nil?
+		tmp = tmp.where('`status` != ?', args[:status!]) unless args[:status!].nil?
 
-    return tmp
-  end
+		return tmp
+	end
 
-  def self.find_by_user_job(user_id, job_id)
-    Application.where('user_id = ? AND job_id = ?', self.user_id, self.job_id).first
-  end
-  
-  def reject
-    self.status = 0
-    self.save
-  end
-    
-  def booked
-    @interviews = Interview.where('user_id = ? AND job_id = ?', self.user_id, self.job_id).first
-  end
+	def self.find_by_user_job(user_id, job_id)
+		Application.where('user_id = ? AND job_id = ?', self.user_id, self.job_id).first
+	end
 
-  def belongs_to_me(user)
-    job=Job.find_by_id(self.job_id)
-    if job != nil 
-      if job.belongs_to_me(user) || job.shared_to_me(user)
-        return true
-      else
-        return false
-      end
-    else
-      return false
-    end
-  end
+	def reject
+		self.status = 0
+		self.save
+	end
+
+	def booked
+		@interviews = Interview.where('user_id = ? AND job_id = ?', self.user_id, self.job_id).first
+	end
+
+	def belongs_to_me(user)
+		job=Job.find_by_id(self.job_id)
+		if job != nil
+			if job.belongs_to_me(user) || job.shared_to_me(user)
+				return true
+			else
+				return false
+			end
+		else
+			return false
+		end
+	end
 
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,15 +1,15 @@
 class Video < ActiveRecord::Base
-  belongs_to :teacher # Migration
-  belongs_to :user
-  has_many :video_views
+	belongs_to :teacher # Migration
+	belongs_to :user
+	has_many :video_views
 
-  # Video Skills
-  has_and_belongs_to_many :skills, :join_table => 'videos_skills'
-  belongs_to :application
-  
-  mount_uploader :video, VideoUploader
-  
-  scope :finished, :conditions => { :encoded_state => "finished" }
+	# Video Skills
+	has_and_belongs_to_many :skills, :join_table => 'videos_skills'
+	belongs_to :application
+
+	mount_uploader :video, VideoUploader
+
+	scope :finished, :conditions => {:encoded_state => "finished"}
 
 	after_create :set_details
 
@@ -20,288 +20,293 @@ class Video < ActiveRecord::Base
 			self.save
 		end
 	end
-  
-  # Encode a new video
-  def encode
-    begin
 
-      # Set I/O file names
-      input = "s3://DemoLessonVideo/#{self.secret_url}"
-      output = "s3://tioki-video-encoded/#{self.id}-#{Time.now.to_i}.mp4"
-      thumbnails = "s3://tioki-video-encoded/#{self.id}-#{Time.now.to_i}/"
+	# Encode a new video
+	def encode
+		begin
 
-      # Create a new zencoder job
-      zen = Zencoder::Job.create({
-        :api_key => 'ebbcf62dc3d33b40a9ac99e623328583',
-        :input => input,
-        :outputs => [
-          {
-            :label => self.id.to_s,
-            :public => true,
-            :url => output,
-            :thumbnails => {
-              :number => 5,
-              :size => "640x480",
-              :base_url => thumbnails
-            }
-          }
-        ]
-      })
+			# Set I/O file names
+			input = "s3://DemoLessonVideo/#{self.secret_url}"
+			output = "s3://tioki-video-encoded/#{self.id}-#{Time.now.to_i}.mp4"
+			thumbnails = "s3://tioki-video-encoded/#{self.id}-#{Time.now.to_i}/"
 
-      # Save the output url, job id, and encoded state
-      self.output_url = zen.body['outputs'][0]['url']
-      self.job_id = zen.body['id'].to_s
-      self.encoded_state = "queued"
+			# Create a new zencoder job
+			zen = Zencoder::Job.create({
+										   :api_key => 'ebbcf62dc3d33b40a9ac99e623328583',
+										   :input => input,
+										   :outputs => [
+											   {
+												   :label => self.id.to_s,
+												   :public => true,
+												   :url => output,
+												   :thumbnails => {
+													   :number => 5,
+													   :size => "640x480",
+													   :base_url => thumbnails
+												   }
+											   }
+										   ]
+									   })
 
-      # Save the model
-      self.save!
+			# Save the output url, job id, and encoded state
+			self.output_url = zen.body['outputs'][0]['url']
+			self.job_id = zen.body['id'].to_s
+			self.encoded_state = "queued"
 
-    # Rescue any errors
-    rescue RuntimeError => exception
-      errors.add_to_base("Video encoding request failed with result: " + exception.to_s)
-      nil
-    end
-  end
+			# Save the model
+			self.save!
 
-  # Encode a new snippet
-  def snippet_encode(time)
-    begin
+				# Rescue any errors
+		rescue RuntimeError => exception
+			errors.add_to_base("Video encoding request failed with result: " + exception.to_s)
+			nil
+		end
+	end
 
-      # Set I/O file names
-      input = "s3://DemoLessonVideo/#{self.secret_url}"
-      output = "s3://tioki-video-encoded/#{self.id}-#{Time.now.to_i}.mp4"
-      thumbnails = "s3://tioki-video-encoded/#{self.id}-#{Time.now.to_i}/"
+	# Encode a new snippet
+	def snippet_encode(time)
+		begin
 
-      # Create a new zencoder job
-      zen = Zencoder::Job.create({
-        :api_key => 'ebbcf62dc3d33b40a9ac99e623328583',
-        :input => input,
-        :outputs => [
-          {
-            :label => self.id.to_s,
-            :public => true,
-            :url => output,
-            :clip_length => "00:00:30.0",
-            :start_clip => time,
-            :thumbnails => {
-              :number => 5,
-              :size => "640x580",
-              :base_url => thumbnails
-            }
-          }
-        ]
-      })
+			# Set I/O file names
+			input = "s3://DemoLessonVideo/#{self.secret_url}"
+			output = "s3://tioki-video-encoded/#{self.id}-#{Time.now.to_i}.mp4"
+			thumbnails = "s3://tioki-video-encoded/#{self.id}-#{Time.now.to_i}/"
 
-      # Save the output url, job id, and encoded state
-      self.output_url = zen.body['outputs'][0]['url']
-      self.job_id = zen.body['id'].to_s
-      self.encoded_state = "queued"
+			# Create a new zencoder job
+			zen = Zencoder::Job.create({
+										   :api_key => 'ebbcf62dc3d33b40a9ac99e623328583',
+										   :input => input,
+										   :outputs => [
+											   {
+												   :label => self.id.to_s,
+												   :public => true,
+												   :url => output,
+												   :clip_length => "00:00:30.0",
+												   :start_clip => time,
+												   :thumbnails => {
+													   :number => 5,
+													   :size => "640x580",
+													   :base_url => thumbnails
+												   }
+											   }
+										   ]
+									   })
 
-      # Save the model
-      self.save!
+			# Save the output url, job id, and encoded state
+			self.output_url = zen.body['outputs'][0]['url']
+			self.job_id = zen.body['id'].to_s
+			self.encoded_state = "queued"
 
-    # Rescue any errors
-    rescue RuntimeError => exception
-      errors.add_to_base("Video encoding request failed with result: " + exception.to_s)
-      nil
-    end
-  end
+			# Save the model
+			self.save!
 
-  # Get the job status
-  def job_status
-    if self.encoded_state != 'finished'
-      status = Zencoder::Job.progress(self.job_id, :api_key => 'ebbcf62dc3d33b40a9ac99e623328583')
+				# Rescue any errors
+		rescue RuntimeError => exception
+			errors.add_to_base("Video encoding request failed with result: " + exception.to_s)
+			nil
+		end
+	end
 
-      # Video is not available
-      if status.code.to_i != 200
+	# Get the job status
+	def job_status
+		if self.encoded_state != 'finished'
+			status = Zencoder::Job.progress(self.job_id, :api_key => 'ebbcf62dc3d33b40a9ac99e623328583')
 
-        short = "Zencoder Job: #{self.job_id} returned `#{status.code}`."
-        unless NOTIFY.nil?
-          NOTIFY.notify!(
-            :short_message => short, :full_message => status.body,
-            :level => 4, :file => __FILE__, :line => __LINE__
-          )
-        else
-          Rails.logger.error(short)
-        end
+			# Video is not available
+			if status.code.to_i != 200
 
-        status = 'unavailable'
-      else
-        status = status.body.state
-      end
+				short = "Zencoder Job: #{self.job_id} returned `#{status.code}`."
+				unless NOTIFY.nil?
+					NOTIFY.notify!(
+						:short_message => short, :full_message => status.body,
+						:level => 4, :file => __FILE__, :line => __LINE__
+					)
+				else
+					Rails.logger.error(short)
+				end
 
-      # Update the encoded state
-      if self.encoded_state != status
-        self.encoded_state = status
-        self.save!
-      end
+				status = 'unavailable'
+			else
+				status = status.body.state
+			end
 
-      # Return the status
-      return status
-    end
+			# Update the encoded state
+			if self.encoded_state != status
+				self.encoded_state = status
+				self.save!
+			end
 
-    return self.encoded_state
-  end
-  
-  # # # # # # # # #
-  # Is this necessary consider deprecating
-  #
-  # def capture_notification(output)
-  #     self.encoded_state = output[:state]
-  #     if self.encoded_state == "finished"
-  #       self.output_url = output[:url]
-  #       self.thumbnail = open(URI.parse("http://" + zencoder_setting["s3_output"]["bucket"] + ".s3.amazonaws.com/thumbnails_#{self.id}/frame_0000.png"))
-  #       self.thumbnail_content_type = "image/png"
-  #       # get the job details so we can retrieve the length of the video in milliseconds
-  #       zen = Zencoder.new
-  #       self.duration_in_ms = zen.details(self.job_id)["job"]["output_media_files"].first["duration_in_ms"]
-  #     end
-  #     self.save
-  #   end
-  # # # # # # # # #
+			# Return the status
+			return status
+		end
 
-  # a handy way to turn duration_in_ms into a formatted string like 5:34
-  def human_length
-    if duration_in_ms
-      minutes = duration_in_ms / 1000 / 60
-      seconds = (duration_in_ms / 1000) - (minutes * 60)
-      sprintf("%d:%02d", minutes, seconds)
-    else
-      "Unknown"
-    end
-  end
+		return self.encoded_state
+	end
 
-  # # # # # # # # # # # # # # # # # # # # #
-  # # # # # # # # # # # # # # # # # # # # #
-  # # # # # # # # # # # # # # # # # # # # #
-  # # # # # # # # # # # # # # # # # # # # #
-  # # # # # # # # # # # # # # # # # # # # #
+	# # # # # # # # #
+	# Is this necessary consider deprecating
+	#
+	# def capture_notification(output)
+	#     self.encoded_state = output[:state]
+	#     if self.encoded_state == "finished"
+	#       self.output_url = output[:url]
+	#       self.thumbnail = open(URI.parse("http://" + zencoder_setting["s3_output"]["bucket"] + ".s3.amazonaws.com/thumbnails_#{self.id}/frame_0000.png"))
+	#       self.thumbnail_content_type = "image/png"
+	#       # get the job details so we can retrieve the length of the video in milliseconds
+	#       zen = Zencoder.new
+	#       self.duration_in_ms = zen.details(self.job_id)["job"]["output_media_files"].first["duration_in_ms"]
+	#     end
+	#     self.save
+	#   end
+	# # # # # # # # #
 
-  # Get the name of the video
-  def name(empty = "Untitled")
-    # Get the name
-    name = super()
+	# a handy way to turn duration_in_ms into a formatted string like 5:34
+	def human_length
+		if duration_in_ms
+			minutes = duration_in_ms / 1000 / 60
+			seconds = (duration_in_ms / 1000) - (minutes * 60)
+			sprintf("%d:%02d", minutes, seconds)
+		else
+			"Unknown"
+		end
+	end
 
-    # Return the name is it is not empty
-    name.nil? || name.empty? ? empty.html_safe : ERB::Util.html_escape(name).html_safe
-  end
+	# # # # # # # # # # # # # # # # # # # # #
+	# # # # # # # # # # # # # # # # # # # # #
+	# # # # # # # # # # # # # # # # # # # # #
+	# # # # # # # # # # # # # # # # # # # # #
+	# # # # # # # # # # # # # # # # # # # # #
 
-  # Get the name of the video
-  def description(empty = "No description...")
+	# Get the name of the video
+	def name(empty = "Untitled")
+		# Get the name
+		name = super()
 
-    # Get the description
-    description = super()
+		# Return the name is it is not empty
+		name.nil? || name.empty? ? empty.html_safe : ERB::Util.html_escape(name).html_safe
+	end
 
-    # If there is no local description then display unavailable for external videos
-    return "Description unavailable for external videos." if external? && (description.nil? || description.empty?)
+	# Get the name of the video
+	def description(empty = "No description...")
 
-    # Return the name is it is not empty
-    description.nil? || description.empty? ? empty.html_safe : ERB::Util.html_escape(description).html_safe
-  end
+		# Get the description
+		description = super()
 
-  # Thumbnails
-  def thumbnail
-    unless external?
-      begin
-        thumbnail_url = self.output_url[0...-4] + '/frame_0001.png'
-        raise StandardError unless HTTParty.get(thumbnail_url).code == 200
-        return thumbnail_url 
-      rescue
-        return "tioki/icons/play-icon.png"
-      end
-    end
+		# If there is no local description then display unavailable for external videos
+		return "Description unavailable for external videos." if external? && (description.nil? || description.empty?)
+
+		# Return the name is it is not empty
+		description.nil? || description.empty? ? empty.html_safe : ERB::Util.html_escape(description).html_safe
+	end
+
+	# Thumbnails
+	def thumbnail
+		unless external?
+			begin
+				thumbnail_url = self.output_url[0...-4] + '/frame_0001.png'
+				raise StandardError unless HTTParty.get(thumbnail_url).code == 200
+				return thumbnail_url
+			rescue
+				return "tioki/icons/play-icon.png"
+			end
+		end
 
 		self.thumbnail_url
-  end
+	end
 
-  # Get the embed code for the video
-  def embed_code(width = nil, height = nil, output_url = nil)
+	# Get the embed code for the video
+	def embed_code(width = nil, height = nil, output_url = nil, wrapper = true)
 
-    # Default scale
-    width = 640 if width.nil?
-    height = 480 if height.nil?
+		# Default scale
+		width = 640 if width.nil?
+		height = 480 if height.nil?
 
-    # Make sure we got a URL
-    output_url = self.output_url if output_url.nil?
+		# Make sure we got a URL
+		output_url = self.output_url if output_url.nil?
 
-    # If the url is external
-    if !output_url.nil? && output_url[0...3] == 'ext'
+		# If the url is external
+		if !output_url.nil? && output_url[0...3] == 'ext'
 
-      # Get the API Response
-      response = details(width, height, output_url)
-      
-      begin
-        # Return HTML Embed Code
-        return response["html"].html_safe
-      rescue
-        self.destroy
-        return nil
-      end
-    end
+			# Get the API Response
+			response = details(width, height, output_url, wrapper)
 
-    if job_status == 'finished'
+			begin
+				# Return HTML Embed Code
+				return response["html"].html_safe
+			rescue
+				self.destroy
+				return nil
+			end
+		end
 
-      # HTML 5 Embed Code
-      embed = <<-EMBED
+		if job_status == 'finished'
+
+			# HTML 5 Embed Code
+			embed = <<-EMBED
       <video id="my_video_1" class="video-js vjs-default-skin" controls preload="auto" width="#{width}" height="#{height}" data-setup="{}">
         <source src="#{output_url}" type='video/mp4'>
       </video>
-      EMBED
+			EMBED
 
-      # Return HTML Embed Code
-      return embed.html_safe
-    elsif job_status == 'unavailable'
+			# Return HTML Embed Code
+			return embed.html_safe
+		elsif job_status == 'unavailable'
 
-      # Return status
-      return "<p class=\"processing\">There was a problem getting the requested video. Please let us know with the blue box in the corner and refererence the page you are on.</p>".html_safe
-    else
+			# Return status
+			return "<p class=\"processing\">There was a problem getting the requested video. Please let us know with the blue box in the corner and refererence the page you are on.</p>".html_safe
+		else
 
-      # Return status
-      return "<p class=\"processing\">The video is currently being processed for web viewage. Please check back in a bit.</p>".html_safe
-    end
-  end
+			# Return status
+			return "<p class=\"processing\">The video is currently being processed for web viewage. Please check back in a bit.</p>".html_safe
+		end
+	end
 
-  # This calls noembed returns no change if the url is local
-  def details(width = 640, height = 480, output_url = nil)
+	# This calls noembed returns no change if the url is local
+	def details(width = 640, height = 480, output_url = nil, wrapper = true)
 
-    # Make sure we got a URL
-    output_url = self.output_url if output_url.nil?
+		# Make sure we got a URL
+		output_url = self.output_url if output_url.nil?
 
-    if !output_url.nil? && output_url[0...3] == 'ext'
-      output_url = output_url[4..-1]
+		if !output_url.nil? && output_url[0...3] == 'ext'
+			output_url = output_url[4..-1]
 
-      # Query noembed
-      return HTTParty.get("http://noembed.com/embed", {
-        :query => {
-          :url => output_url,
-          :maxwidth => width,
-          :maxheight => height
-        }
-      })
-    end
+			# Query noembed
+			query = {
+				:url => output_url,
+				:maxwidth => width,
+				:maxheight => height,
+			}
 
-    return output_url
-  end
+			# Should we wrap the embed?
+			query[:nowrap] = :on unless wrapper
 
-  # Is video external to tioki
-  def external?
-    return false if self.output_url.nil? || self.output_url.empty?
-    self.output_url[0...3] == 'ext' ? true : false
-  end
+			return HTTParty.get("http://noembed.com/embed", {
+				:query => query
+			})
+		end
 
-  # Cleanup
-  def cleanup
-  end
+		return output_url
+	end
 
-  # Private methods below
-  private
+	# Is video external to tioki
+	def external?
+		return false if self.output_url.nil? || self.output_url.empty?
+		self.output_url[0...3] == 'ext' ? true : false
+	end
 
-    def zencoder_setting
-      @zencoder_config ||= YAML.load_file("#{Rails.root}/config/zencoder.yml")
-    end
-    
-    def video_changed?
-      return false
-    end
-    
+	# Cleanup
+	def cleanup
+	end
+
+	# Private methods below
+	private
+
+	def zencoder_setting
+		@zencoder_config ||= YAML.load_file("#{Rails.root}/config/zencoder.yml")
+	end
+
+	def video_changed?
+		return false
+	end
+
 end
