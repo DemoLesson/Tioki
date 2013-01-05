9c7472ba549f6bc1e3f2b60e6140b999bb7ee9a1
Kelly Becker <kellylsbkr@gmail.com>
2013-01-04 21:11:09 -0800

    newrubber_profile

--- config/rubber/deploy-setup.rb
+++ config/rubber/deploy-setup.rb
@@ -38,9 +38,65 @@ namespace :rubber do
     end
   end
 
+###############
+# System Ruby #
+###############
+  namespace :base do
+  
+    rubber.allow_optional_tasks(self)
+
+    before "rubber:setup_gem_sources", "rubber:base:install_ruby"
+    task :install_ruby do
+      rubber.sudo_script "install_ruby", <<-ENDSCRIPT
+        # Go ahead and set ruby to the 1.9.1 version instead of the default 1.8.*
+        rm /etc/alternatives/ruby
+        ln /usr/bin/ruby1.9.1 /etc/alternatives/ruby
+
+        # Install bundler
+        gem install bundler
+      ENDSCRIPT
+    end
+
+    after "rubber:install_packages", "rubber:base:configure_git" if scm == "git"
+    task :configure_git do
+      rubber.sudo_script 'configure_git', <<-ENDSCRIPT
+        if [[ "#{repository}" =~ "@" ]]; then
+          # Get host key for src machine to prevent ssh from failing
+          rm -f ~/.ssh/known_hosts
+          ssh -o 'StrictHostKeyChecking=no' #{repository.gsub(/:.*/, '')} &> /dev/null || true
+        fi
+      ENDSCRIPT
+    end
+    
+    # We need a rails user for safer permissions used by deploy.rb
+    after "rubber:install_packages", "rubber:base:custom_install"
+    task :custom_install do
+      rubber.sudo_script 'custom_install', <<-ENDSCRIPT
+        # add the user for running app server with
+        if ! id #{rubber_env.app_user} &> /dev/null; then adduser --system --group #{rubber_env.app_user}; fi
+          
+        # add ssh keys for root 
+        if [[ ! -f /root/.ssh/id_dsa ]]; then ssh-keygen -q -t dsa -N '' -f /root/.ssh/id_dsa; fi
+      ENDSCRIPT
+    end
+
+    # Update /etc/sudoers so that SSH-related environment variables so capistrano/rubber tasks can take advantage of ssh-agent forwarding
+    before "rubber:bootstrap", "rubber:base:update_sudoers"
+    task :update_sudoers do
+      rubber.sudo_script "update_sudoers", <<-ENDSCRIPT
+        if [[ ! `grep 'SSH_CLIENT SSH_TTY SSH_CONNECTION SSH_AUTH_SOCK' /etc/sudoers` =~ "SSH_CLIENT SSH_TTY SSH_CONNECTION SSH_AUTH_SOCK" ]]; then
+          echo '' >> /etc/sudoers
+          echo '# whitelist SSH-related environment variables so capistrano tasks can take advantage of ssh-agent forwarding' >> /etc/sudoers
+          echo 'Defaults env_keep += "SSH_CLIENT SSH_TTY SSH_CONNECTION SSH_AUTH_SOCK"' >> /etc/sudoers
+        fi
+      ENDSCRIPT
+    end
+  end
+
 #######
 # RVM #
 #######
+=begin
   namespace :base do
   
     rubber.allow_optional_tasks(self)
@@ -157,6 +213,7 @@ namespace :rubber do
       ENDSCRIPT
     end
   end
+=end
 
 ##############
 # Ruby Build #
