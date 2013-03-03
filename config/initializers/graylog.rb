03e266288a0f3b99f825c1a949473395fe00711f
Elijah Green <elijahgreen@gmail.com>
2013-03-03 00:16:07 -0800

    Don't log development errors to graylog

--- config/initializers/graylog.rb
+++ config/initializers/graylog.rb
@@ -1,68 +1,70 @@
 require 'gelf'
+if Rails.env != "development"
 
-class MultiLogger
-  def initialize(*objects)
-    @objects = objects
-  end
+	class MultiLogger
+		def initialize(*objects)
+			@objects = objects
+		end
 
-  def method_missing(*args)
-    @objects.each {|o| o.send(*args) }
-  end
-end
-
-glconfig = Hash.new
-glconfig[:server] = "graylog2.tioki.com"
-glconfig[:port] = 12201
-glconfig[:host] = `hostname`
-
-# Check for server response
-response = HTTParty.get("http://#{glconfig[:server]}/login").code == 200 rescue false
-
-# If a valid response was received create a gelf logger
-if response
-	gelf_logger = GELF::Logger.new(glconfig[:server],
-	glconfig[:port],
-	'LAN',
-	'facility' => 'rails',
-	'host' => glconfig[:host]
-	)
-	Rails.logger = MultiLogger.new(Rails.logger, gelf_logger)
-
-	# Notify is more detailed the Rails Logger
-	NOTIFY = GELF::Notifier.new(glconfig[:server],
-	glconfig[:port],
-	'LAN',
-	'facility' => 'rails',
-	'host' => glconfig[:host]
-	)
-	NOTIFY.collect_file_and_line = false
-
-	# See https://github.com/Graylog2/graylog2_exceptions/wiki
-	Rails.application.config.middleware.use "Graylog2Exceptions",
-	{
-	  :hostname => glconfig[:server],
-	  :port => glconfig[:port],
-	  :facility => "rails_exceptions",
-	  :local_app_name => glconfig[:host],
-	  :level => GELF::FATAL,
-	  :max_chunk_size => 'LAN'
-	}
-
-	# Setup logging unhandled resque exceptions to graylog
-	Graylog2::Resque::FailureHandler.configure do |config|
-	config.gelf_server = glconfig[:server]
-	config.gelf_port = glconfig[:port]
-	config.host = glconfig[:host]
-	config.facility = "resque_exceptions"
-	config.level = GELF::FATAL
-	config.max_chunk_size = 'LAN'
+		def method_missing(*args)
+			@objects.each {|o| o.send(*args) }
+		end
 	end
 
-	require 'resque/failure/multiple'
-	require 'resque/failure/redis'
-	Resque::Failure::Multiple.classes = [
-	  Graylog2::Resque::FailureHandler,
-	  Resque::Failure::Redis
-	]
-	Resque::Failure.backend = Resque::Failure::Multiple
+	glconfig = Hash.new
+	glconfig[:server] = "graylog2.tioki.com"
+	glconfig[:port] = 12201
+	glconfig[:host] = `hostname`
+
+	# Check for server response
+	response = HTTParty.get("http://#{glconfig[:server]}/login").code == 200 rescue false
+
+	# If a valid response was received create a gelf logger
+	if response
+		gelf_logger = GELF::Logger.new(glconfig[:server],
+																	 glconfig[:port],
+																	 'LAN',
+																	 'facility' => 'rails',
+																	 'host' => glconfig[:host]
+																	)
+		Rails.logger = MultiLogger.new(Rails.logger, gelf_logger)
+
+		# Notify is more detailed the Rails Logger
+		NOTIFY = GELF::Notifier.new(glconfig[:server],
+																glconfig[:port],
+																'LAN',
+																'facility' => 'rails',
+																'host' => glconfig[:host]
+															 )
+		NOTIFY.collect_file_and_line = false
+
+		# See https://github.com/Graylog2/graylog2_exceptions/wiki
+		Rails.application.config.middleware.use "Graylog2Exceptions",
+			{
+			:hostname => glconfig[:server],
+			:port => glconfig[:port],
+			:facility => "rails_exceptions",
+			:local_app_name => glconfig[:host],
+			:level => GELF::FATAL,
+			:max_chunk_size => 'LAN'
+		}
+
+			# Setup logging unhandled resque exceptions to graylog
+			Graylog2::Resque::FailureHandler.configure do |config|
+				config.gelf_server = glconfig[:server]
+				config.gelf_port = glconfig[:port]
+				config.host = glconfig[:host]
+				config.facility = "resque_exceptions"
+				config.level = GELF::FATAL
+				config.max_chunk_size = 'LAN'
+			end
+
+			require 'resque/failure/multiple'
+			require 'resque/failure/redis'
+			Resque::Failure::Multiple.classes = [
+				Graylog2::Resque::FailureHandler,
+				Resque::Failure::Redis
+			]
+			Resque::Failure.backend = Resque::Failure::Multiple
+	end
 end
