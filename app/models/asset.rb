b6dcb1b89239c971ba1d2e9ed177d9e8ec317b76
Elijah Green <elijahgreen@gmail.com>
2013-03-03 16:51:00 -0800

    Replace depreciated function

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -27,7 +27,7 @@ class Asset < ActiveRecord::Base
 			extension = File.extname(file_file_name)
 			basename = File.basename(file_file_name, extension)
 			self.name = file_file_name
-			self.file.instance_write(:file_name, "#{basename}_#{ActiveSupport::SecureRandom.hex(16)}#{extension}")
+			self.file.instance_write(:file_name, "#{basename}_#{SecureRandom.hex(16)}#{extension}")
 		end
 
 		# Delete file off S3
