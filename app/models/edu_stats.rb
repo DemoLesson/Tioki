8ee7a166975d4366612fdd41c3d826466c12df0e
Elijah Green <elijahgreen@gmail.com>
2013-03-20 19:12:23 -0700

    Add some more edu stats validations

--- app/models/edu_stats.rb
+++ app/models/edu_stats.rb
@@ -1,8 +1,11 @@
 class EduStats < ActiveRecord::Base
 	belongs_to :user
-	validates :yrs_teaching, :numericality => {:only_integer => true}
-	validates :avg_class_size, :numericality => {:only_integer => true}
-	validates :class_perday, :numericality => {:only_integer => true}
+	validates :yrs_teaching,
+	          :numericality => {:only_integer => true, :greater_than_or_equal_to => 1}
+	validates :avg_class_size,
+	          :numericality => {:only_integer => true, :greater_than_or_equal_to => 1}
+	validates :class_perday,
+	          :numericality => {:only_integer => true, :greater_than_or_equal_to => 1}
 end
 
 
